c4f1dd66b6cdd344838ece9d16cf1379415f83ad
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-09-13 06:02:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 // Your code here
-let 
+const mapToNegativize=(sourceArray)=>{
+    return sourceArray.map(element=>-element);
+}
+const mapToNoChange=(sourceArray)=>{
+    return sourceArray.map(element=>element);
+}
+const mapToDouble=(sourceArray)=>{
+    return sourceArray.map(element=>2*element);
+}
+const mapToSquare=(sourceArray)=>{
+    return sourceArray.map(element=>element**2);
+}
